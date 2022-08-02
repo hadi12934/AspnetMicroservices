6f168fa201f714a9a53c3ee3fc562ade4e2e19d2
Hadi Farahani <h.farahani@H-FARAHANI-PRD>
2022-08-02 12:17:07 +0430

    CatalogContext

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DaabaseSettings:ConnectionStirng"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DaabaseSettings:DatabaseName"));
+            var Products = database.GetCollection<Product>(configuration.GetValue<string>("DaabaseSettings:CollectionName"));
+            CatalogContextSeed.SeddDate(Products);
+        }
+        public IMongoCollection<Product> Products => throw new NotImplementedException();
+    }
+}
